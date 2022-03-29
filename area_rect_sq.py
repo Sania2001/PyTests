db0770763a3589bb844b3be36fd9f87e0989d896
Sania2001 <saniagshinde@gmail.com>
2022-03-29 14:46:19 +0530

    .

--- area_rect_sq.py
+++ area_rect_sq.py
@@ -37,3 +37,4 @@ if __name__ == "__main__":
 
         else:
             print("Wrong")
+            break;
--- test_rect_sq.py
+++ test_rect_sq.py
@@ -1,17 +1,21 @@
+import pytest
 import area_rect_sq
 
+@pytest.mark.skip(reason = "Currently skipped")
 def test_area_rect():
     x =10
     y =20
     result = area_rect_sq.area_rect(x,y)
     assert x*y == result
 
+@pytest.mark.myproject
 def test_peri():
     x =10
     y =20
     result = area_rect_sq.pri_rect(x,y)
     assert x+x+y+y == result
 
+@pytest.mark.myproject
 def test_area_sq():
     x =10
     result = area_rect_sq.area_sq(x)
