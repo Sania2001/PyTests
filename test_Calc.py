df05c98fd5538b40eeb2320628188a8e26f365cc
Sania2001 <saniagshinde@gmail.com>
2022-03-29 15:42:20 +0530

    .

--- test_Calc.py
+++ test_Calc.py
@@ -1,25 +1,30 @@
 import Calc
+import pytest
 
-def test_add():
-    x =10
-    y =20
-    result = Calc.add(x,y)
-    assert x+y == result
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(5,6,11),(4,5,6)])
+def test_add(a,b,c):
+    # x =10
+    # y =20
+    result = Calc.add(a,b)
+    assert c == result
 
-def test_sub():
-    x =20
-    y =10
-    result = Calc.sub(x,y)
-    assert x-y == result
+@pytest.mark.parametrize("a,b,c",[(5,3,2),(11,5,6),(4,5,6)])
+def test_sub(a,b,c):
+    # x =20
+    # y =10
+    result = Calc.sub(a,b)
+    assert c == result
 
-def test_mul():
-    x = 20
-    y = 10
-    result = Calc.mul(x, y)
-    assert x * y == result
+@pytest.mark.parametrize("a,b,c",[(5,3,15),(11,5,55),(4,5,6)])
+def test_mul(a,b,c):
+    # x = 20
+    # y = 10
+    result = Calc.mul(a, b)
+    assert c == result
 
-def test_div():
-    x = 20
-    y = 10
-    result = Calc.div(x, y)
-    assert x / y == result
+@pytest.mark.parametrize("a,b,c",[(4,2,2),(36,6,6),(4,5,6)])
+def test_div(a,b,c):
+    # x = 20
+    # y = 10
+    result = Calc.div(a, b)
+    assert c == result
